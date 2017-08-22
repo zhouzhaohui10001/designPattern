ff48d24714a8ff62de187a8a9646290ed255f5cf
zhouzhaohui <zhouzhaohui@gomeplus.com>
2017-08-22 15:50:23 +0800

    自动拆装箱测试

--- src/test/java/AutoPackTest.java
+++ src/test/java/AutoPackTest.java
@@ -8,5 +8,20 @@ public class AutoPackTest {
         Integer i2 = new Integer(1);
         int i3 =1;
         System.out.println(i1==i2);
+        System.out.println(i1==i3);
+        System.out.println(i2==i3);
+        Integer i4 = new Integer(145);
+        int i5 = 145;
+        System.out.println(i4==i5);
+        Integer i6 = 127;
+        int i7 = 127;
+        System.out.println(i6==i7);
+        Integer i8 = 128;
+        int i9 = 128;
+        System.out.println(i8==i9);
+        Integer i10 = 127;
+        System.out.println(i10==i6);
+        Integer i11= 128;
+        System.out.println(i11==i8);
     }
 }
