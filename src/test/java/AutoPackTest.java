bb147479c062ad776cc5b5cec24ba6963bd5ba5e
zhouzhaohui <zhouzhaohui@gomeplus.com>
2017-08-22 14:28:47 +0800

    自动拆装箱测试

---
+++ src/test/java/AutoPackTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by zhouzhaohui on 2017/8/22.
+ */
+public class AutoPackTest {
+
+    public static void main(String[] args) {
+        Integer i1 = new Integer(1);
+        Integer i2 = new Integer(1);
+        int i3 =1;
+        System.out.println(i1==i2);
+    }
+}
